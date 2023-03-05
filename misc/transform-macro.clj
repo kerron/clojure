1c84ab3a206eb54869c10714f04f06eae3f8d081
Kerron Parchment <kerron68@gmail.com>
2023-03-05 19:18:44 +0000

    chore: move transform-marco.clj

--- misc/transform-macro.clj
+++ misc/transform-macro.clj
@@ -1,42 +1,37 @@
 ;; most common words in English
-(def common-word 
+(def common-word
   (-> (slurp "https://www.textfixer.com/tutorials/common-english-words.txt")
       (clojure.string/split #",")
       set))
 
-
 ;; save book in a variable
-(def book 
+(def book
   (slurp "https://raw.githubusercontent.com/jraleman/42_get_next_line/master/tests/hhgttg.txt"))
-
 
 (def words (re-seq #"[\w|']+" book))
 ;; => ("The" "Hitch" "Hiker's" "Guide" "to" "the" "Galaxy"...) 
 
-
 ;; 4 most frequently used words
-(defn most-common-words [text] 
+(defn most-common-words [text]
   (->> text
-    (map clojure.string/lower-case)
-    (remove common-word)
-    frequencies
-    (sort-by val)
-    (take-last 4)
-    reverse))
+       (map clojure.string/lower-case)
+       (remove common-word)
+       frequencies
+       (sort-by val)
+       (take-last 4)
+       reverse))
 
 (most-common-words words)
 ;; => (["arthur" 332] ["ford" 316] ["zaphod" 206] ["up" 180])
 
-
 ;; 4 longest words
-(defn longest-words [text] 
-  (->> words 
-     (distinct)
-     (sort-by count)
-     (take-last 4)
-     (group-by count)
-     reverse))
-
+(defn longest-words [text]
+  (->> words
+       (distinct)
+       (sort-by count)
+       (take-last 4)
+       (group-by count)
+       reverse))
 
 (longest-words words)
 ;; => ([43 ["Nnnnnnnnnnyyyyyyyuuuuuuurrrrrrrggggggghhhhh"]] 
@@ -45,12 +40,12 @@
        ;;[19 ["Aaaaaaarggggghhhhhh"]])
 
 ;; typical clojure expression
-(clojure.walk/macroexpand-all '(->> book 
-                                    (re-seq #"[\w|']+") 
+(clojure.walk/macroexpand-all '(->> book
+                                    (re-seq #"[\w|']+")
                                     (map #(clojure.string/lower-case %))
-                                    (remove common-word) 
-                                    frequencies 
-                                    (sort-by val) 
+                                    (remove common-word)
+                                    frequencies
+                                    (sort-by val)
                                     reverse))
 ;; (reverse (sort-by val (frequencies (remove common-word (map (fn* [%1] (clojure.string/lower-case %1)) (re-seq #"[\w|']+" book))))))
 
