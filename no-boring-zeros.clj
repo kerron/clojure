46b1111c1317cb412d0bf6ebee27ce7d7272c39b
Kerron Parchment <kerron68@gmail.com>
2023-03-05 14:54:48 +0000

    feat: add no-boring-zeros.clj challenge

---
+++ no-boring-zeros.clj
@@ -0,0 +1,7 @@
+;; https://www.codewars.com/kata/570a6a46455d08ff8d001002/clojure
+;; remove the last digit if it is zero, and return zero if the number has only one digit and it's zero
+
+(defn no-boring-zeros [n]
+  (cond (= n 0) 0
+        (= (mod n 10) 0) (no-boring-zeros (quot n 10))
+        :else n))
