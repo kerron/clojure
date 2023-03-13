dab5b742be68723863613c2bba76f1c7ae865486
Kerron Parchment <kerron68@gmail.com>
2023-03-13 21:23:25 +0000

    feat: add your_order_please.clj

---
+++ challenges/your_order_please.clj
@@ -0,0 +1,26 @@
+(ns your-order-please
+  (:require [clojure.string :as str]))
+
+;  https://www.codewars.com/kata/55c45be3b2079eccff00010f
+
+; Your task is to sort a given string. 
+; Each word in the string will contain a single number. 
+; This number is the position the word should have in the result.
+
+; Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+; If the input string is empty, return an empty string. 
+; The words in the input String will only contain valid consecutive numbers
+
+; "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+; "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+; ""  -->  ""
+
+(defn your-order-please [s]
+  (-> s
+      (clojure.string/split #" ")
+      (->>
+       (sort-by #(Integer/parseInt (re-find #"\d+" %)))
+       (clojure.string/join " "))))
+
+(your-order-please "is2 Thi1s T4est 3a") ;;=> "Thi1s is2 3a T4est"
