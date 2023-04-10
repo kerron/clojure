ca9ca4f5c9a8194d325d7632b21602236611326f
Kerron Parchment <kerron68@gmail.com>
2023-04-10 20:23:43 +0100

    feat: add encode_dups.clj

---
+++ challenges/encode_dups.clj
@@ -0,0 +1,24 @@
+(ns challenges.encode-dups
+  (:require [clojure.string :as str]))
+;; https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/clojure
+;; The goal of this exercise is to convert a string to a
+;; new string where each character in the new string is
+;; "(" if that character appears only once in the original 
+;; string, or ")" if that character appears more than 
+;; once in the original string. Ignore capitalization 
+;; when determining if a character is a duplicate.
+;;
+;; Examples
+;; "din"      =>  "((("
+;; "recede"   =>  "()()()"
+;; "Success"  =>  ")())())"
+;; "(( @"     =>  "))((" 
+
+(defn encode-dups [input]
+  (let [freq-map (->> input
+                      str/lower-case
+                      frequencies)
+        lowercase-input (str/lower-case input)]
+    (apply str (mapv #(if (> (get freq-map %) 1) ")" "(") lowercase-input))))
+
+(encode-dups "kerron") ;; => "(())(("
