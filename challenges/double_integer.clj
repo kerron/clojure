ef1fbc95ce8aca559320454cab4f93c0c1651498
Kerron Parchment <kerron68@gmail.com>
2023-04-16 11:04:25 +0100

    feat: add double_integer.clj

---
+++ challenges/double_integer.clj
@@ -0,0 +1,8 @@
+(ns challenges.double-integer)
+;; https://www.codewars.com/kata/53ee5429ba190077850011d4
+;; Code as fast as you can! You need to double the integer and return it.
+
+(defn double-integer [x]
+  (* x 2))
+
+(double-integer 2) ; => 4
