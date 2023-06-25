9a1580cf3c0deeb1a21a39470babe4a11160368a
Kerron Parchment <kerron68@gmail.com>
2023-06-25 21:59:36 +0100

    feat: add digital_root.clj

---
+++ challenges/digital_root.clj
@@ -0,0 +1,13 @@
+;; Digital root is the recursive sum of all the digits in a number.
+;; Given n, take the sum of the digits of n.
+;; If that value has more than one digit, continue reducing in this
+;; way until a single-digit number is produced. The input will be a non-negative integer.
+;; 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+
+(ns digital-root)
+
+(defn digit-root [n]
+  (let [sum (reduce + (map #(Character/digit % 10) (str n)))]
+    (if (> sum 9)
+      (recur sum)
+      sum)))
